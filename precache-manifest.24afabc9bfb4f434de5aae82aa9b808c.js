fedde00bea1c67dfcf5a34d00f13172193e5f89a
nparida3 <61738434+nparida3@users.noreply.github.com>
2020-06-04 17:21:44 +0530

    Updates

---
+++ precache-manifest.24afabc9bfb4f434de5aae82aa9b808c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1b04108aee9a144a77323c75e1c530",
+    "url": "/recipestore/index.html"
+  },
+  {
+    "revision": "aea10c807d77c9d4fcb1",
+    "url": "/recipestore/static/css/main.0cbc2c2e.chunk.css"
+  },
+  {
+    "revision": "cc4d4ff9f505b2e65842",
+    "url": "/recipestore/static/js/2.b489cf4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipestore/static/js/2.b489cf4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aea10c807d77c9d4fcb1",
+    "url": "/recipestore/static/js/main.00be2d8c.chunk.js"
+  },
+  {
+    "revision": "e7aafed5f47e10ce5599",
+    "url": "/recipestore/static/js/runtime-main.b565ea0e.js"
+  }
+]);
